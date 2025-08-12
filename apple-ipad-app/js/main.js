0cf112b75a814e10fc503a4a2683a98001af48f5
Azuking69 <azuking69@hotmail.co.jp>
2025-08-12 13:42:45 +0900

    25.08.12 count1

---
+++ apple-ipad-app/js/main.js
@@ -0,0 +1,15 @@
+const basketStarterEl = document.querySelector('header .basket-starter')
+const basketEl = basketStarterEl.querySelector('.basket')
+
+basketStarterEl.addEventListener('click', function(event){
+    event.stopPropagation()
+    if (basketEl.classList.contains('show')) {
+        basketEl.classList.remove('show')
+    } else {
+        basketEl.classList.add('show')
+    }
+})
+
+window.addEventListener('click', function(){
+    basketEl.classList.remove('show')
+})
